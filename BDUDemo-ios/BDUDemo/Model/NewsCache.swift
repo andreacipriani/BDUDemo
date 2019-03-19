7cc1bfca8aa92c7da84c843b2b1d4c3614912a76
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2019-03-19 16:55:07 +0100

    Push json copies

--- BDUDemo-ios/BDUDemo/Model/NewsCache.swift
+++ BDUDemo-ios/BDUDemo/Model/NewsCache.swift
@@ -2,6 +2,22 @@ import Foundation
 
 // Simulates a database
 
-struct NewsCache {
-  
+protocol NewsCaching {
+    func newsText(for id: String) -> String
 }
+
+struct NewsCache: NewsCaching {
+  
+    func newsText(for id: String) -> String {
+        
+    }
+}
+
+TODO:
+
+- news details
+- ad details with view
+- another cell from the beginning
+- another theme
+- unit tests
+- ui tests 
